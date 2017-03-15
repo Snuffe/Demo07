4a5d13ee61e75ae072a815d521c675c92ffa9201
Snuffe <K1493@student.jamk.fi>
2017-03-15 23:35:36 +0200

    lottokone ilman "row"nr""

---
+++ Demo07/Lottokone/Lotto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottokone
+{
+    class Lotto
+    {
+        private Random rand = new Random();
+        private int Number;
+        private List<int> Numbers = new List<int>();
+        private List<string> NumbersString = new List<string>();
+        private List<string> Rows = new List<string>();
+
+        public string GetRows(int RowN)
+        {
+            for (int i = 0; i < RowN; i++)
+            {
+                while (Numbers.Count < 7) 
+                {
+                    Number = rand.Next(1, 39 + 1);
+                    if(!Numbers.Contains(Number))
+                    {
+                        Numbers.Add(Number);
+                    }
+                }
+
+                for (int j = 0; j < 7; j++)
+                {
+                    if (Numbers[j] < 10) //jos luku j on alle 10
+                    {
+                        NumbersString.Add("0" + Numbers[j].ToString()); //lisätään luvun eteen 0 joten luvusta 7 tulee 07
+                    }
+                    else
+                    {
+                        NumbersString.Add(Numbers[j].ToString()); //muuten lisätään luku j lukujonoon
+                    }
+                }
+                NumbersString.Sort(); //järjestää numerot pienimmästä suurimpaan
+                Rows.Add(string.Join(" ", NumbersString));
+                Numbers.Clear();
+                NumbersString.Clear();
+            }
+            return string.Join(Environment.NewLine, Rows);
+        }
+
+    }
+}
--- Demo07/Lottokone/MainPage.xaml.cs
+++ Demo07/Lottokone/MainPage.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,33 +24,31 @@ namespace Lottokone
         public MainPage()
         {
             this.InitializeComponent();
-
-            //change the default startup mode
-            ApplicationView.PreferredLaunchWindowingMode
-                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-            //window size 800x600
-            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
-            App.Current.DebugSettings.EnableFrameRateCounter = false;
         }
 
-        private void drawButton_Click(object sender, RoutedEventArgs e)
+        private void Draw_Click(object sender, RoutedEventArgs e)
         {
-            Random rand = new Random();
-
-            string num = "";
-            for(textBox.Text)
-            for (int x = 0; x < 7; x++)
+            DrawsTextBlock.Text = "";
+            int RowsAmount;
+            try
             {
-                
-                int number = rand.Next(1, 39 + 1);
-                num += number+ ",";
-                textBlock2.Text = num;
+                RowsAmount = int.Parse(Amount.Text);
+            }
+            catch
+            {
+                RowsAmount = 0;
+            }
+            if (RowsAmount != 0)
+            {
+                Lotto Lotto = new Lotto();
+                string Rows = Lotto.GetRows(RowsAmount);
+                DrawsTextBlock.Text += Rows + System.Environment.NewLine;
             }
         }
 
-        private void clearButton_Click(object sender, RoutedEventArgs e)
+        private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            textBlock2.Text = "";
+            DrawsTextBlock.Text = "";
         }
     }
 }
